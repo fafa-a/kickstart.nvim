ed434ed1efd586ae0ad97897c0eb677c82146e05
fafa-a <fabienpuech1@gmail.com>
2023-08-17 17:47:46 +0200

    chore: handle better files config

---
+++ after/plugin/keymap.lua
@@ -0,0 +1,147 @@
+local map = vim.keymap.set
+-- -- format and save
+map("n", "<C-s>", ":w<CR>", { silent = true })
+
+-- hanldes escape key & fixed copilot suggestion
+map("i", "<C-c>", "<Esc>")
+-- map("n", "<C-s>", ":w<CR>:Format<CR>", { silent = true })
+
+-- close current buffer
+map("n", "<leader>c", ":bd<CR>", { silent = true })
+
+-- buffer navigation
+map("n", "<S-b>", ":bprev<CR>", { silent = true })
+map("n", "<S-n>", ":bnext<CR>", { silent = true })
+
+-- lazygit
+map("n", "<leader>gg", ":LazyGit<CR>", { silent = true })
+
+-- toggle nvim-tree explorer
+map("n", "<leader>x", ":Neotree toggle<CR>", { silent = true })
+
+--disable arrow keys
+map("n", "<up>", "<nop>", { silent = true })
+map("n", "<down>", "<nop>", { silent = true })
+map("n", "<left>", "<nop>", { silent = true })
+map("n", "<right>", "<nop>", { silent = true })
+
+-- keymap for moving
+map("n", "<C-j>", "<C-w>j", { silent = true })
+map("n", "<C-k>", "<C-w>k", { silent = true })
+map("n", "<C-l>", "<C-w>l", { silent = true })
+map("n", "<C-h>", "<C-w>h", { silent = true })
+
+--trouble settings
+map("n", "<leader>xx", "<cmd>TroubleToggle<cr>", { silent = true, noremap = true })
+map("n", "<leader>xw", "<cmd>TroubleToggle workspace_diagnostics<cr>", { silent = true, noremap = true })
+map("n", "<leader>xd", "<cmd>TroubleToggle document_diagnostics<cr>", { silent = true, noremap = true })
+map("n", "<leader>xl", "<cmd>TroubleToggle loclist<cr>", { silent = true, noremap = true })
+map("n", "<leader>xq", "<cmd>TroubleToggle quickfix<cr>", { silent = true, noremap = true })
+map("n", "gR", "<cmd>TroubleToggle lsp_references<cr>", { silent = true, noremap = true })
+
+-- copilot accept suggestion with tab
+map("i", "<tab>", function()
+	require("copilot.suggestion").accept()
+	-- Put cursor on next line.
+	vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<End>", true, false, true), "n", false)
+end, { desc = "[copilot] accept suggestion", silent = true })
+
+
+-- lsp lines
+map("", "<Leader>l", require("lsp_lines").toggle, { desc = "Toggle lsp_lines" })
+
+-- lspsaga
+
+-- LSP finder - Find the symbol's definition
+-- If there is no definition, it will instead be hidden
+-- When you use an action in finder like "open vsplit",
+-- you can use <C-t> to jump back
+map("n", "gh", "<cmd>Lspsaga lsp_finder<CR>")
+
+-- Code action
+map({ "n", "v" }, "<leader>ca", "<cmd>Lspsaga code_action<CR>")
+
+-- Rename all occureences of the hovered word for the entire file
+map("n", "gr", "<cmd>Lspsaga rename<CR>")
+
+-- Rename all occurrences of the hovered word for the selected files
+--map("n", "gr", "<cmd>Lspsaga rename ++project<CR>")
+
+-- Peek definition
+-- You can edit the file containing the definition in the floating window
+-- It also supports open/vsplit/etc operations, do refer to "definition_action_keys"
+-- It also supports tagstack
+-- Use <C-t> to jump back
+map("n", "gp", "<cmd>Lspsaga peek_definition<CR>")
+
+-- Go to definition
+map("n", "gd", "<cmd>Lspsaga goto_definition<CR>")
+
+-- Peek type definition
+-- You can edit the file containing the type definition in the floating window
+-- It also supports open/vsplit/etc operations, do refer to "definition_action_keys"
+-- It also supports tagstack
+-- Use <C-t> to jump back
+map("n", "gt", "<cmd>Lspsaga peek_type_definition<CR>")
+
+-- Go to type definition
+map("n", "gt", "<cmd>Lspsaga goto_type_definition<CR>")
+
+
+-- Show line diagnostics
+-- You can pass argument ++unfocus to
+-- unfocus the show_line_diagnostics floating window
+map("n", "<leader>sl", "<cmd>Lspsaga show_line_diagnostics<CR>")
+
+-- Show buffer diagnostics
+map("n", "<leader>sb", "<cmd>Lspsaga show_buf_diagnostics<CR>")
+
+-- Show workspace diagnostics
+map("n", "<leader>sw", "<cmd>Lspsaga show_workspace_diagnostics<CR>")
+
+-- Show cursor diagnostics
+map("n", "<leader>sc", "<cmd>Lspsaga show_cursor_diagnostics<CR>")
+-- Diagnostic jump with filters such as only jumping to an error
+map("n", "[E", function()
+	require("lspsaga.diagnostic"):goto_prev({ severity = vim.diagnostic.severity.ERROR })
+end)
+map("n", "]E", function()
+	require("lspsaga.diagnostic"):goto_next({ severity = vim.diagnostic.severity.ERROR })
+end)
+
+-- Toggle outline
+map("n", "<leader>o", "<cmd>Lspsaga outline<CR>")
+
+-- Hover Doc
+-- If there is no hover doc,
+-- there will be a notification stating that
+-- there is no information available.
+-- To disable it just use ":Lspsaga hover_doc ++quiet"
+-- Pressing the key twice will enter the hover window
+map("n", "K", "<cmd>Lspsaga hover_doc<CR>")
+
+-- Call hierarchy
+map("n", "<Leader>ci", "<cmd>Lspsaga incoming_calls<CR>")
+map("n", "<Leader>co", "<cmd>Lspsaga outgoing_calls<CR>")
+
+-- Floating terminal
+map({ "n", "t" }, "<A-d>", "<cmd>Lspsaga term_toggle<CR>")
+
+-- use ripgrep to search
+local live_grep_args_shortcuts = require("telescope-live-grep-args.shortcuts")
+map("n", "<leader>gc", live_grep_args_shortcuts.grep_word_under_cursor)
+map("v", "<leader>gs", live_grep_args_shortcuts.grep_visual_selection)
+
+-- -- harpoon settings
+local mark = require("harpoon.mark")
+local ui = require("harpoon.ui")
+
+map("n", "<leader>aa", mark.add_file)
+map("n", "<C-n>", ui.toggle_quick_menu)
+
+map("n", "<C-;>", function() ui.nav_file(1) end)
+map("n", "<C-'>", function() ui.nav_file(2) end)
+map("n", "<S-3>", function() ui.nav_file(3) end)
+map("n", "<S-4>", function() ui.nav_file(4) end)
+
+
---
+++ after/plugin/options.lua
@@ -0,0 +1,125 @@
+local opt = vim.opt
+-- Decrease update time
+opt.updatetime = 50
+vim.wo.signcolumn = "yes"
+
+opt.completeopt = { "menu", "menuone", "noselect" }
+-- relative line numbers
+opt.relativenumber = true
+
+-- confirm before closing unsaved buffer
+opt.confirm = true
+
+-- search acting like modern browsers
+opt.incsearch = true
+
+-- space before end of 'viewport'
+opt.scrolloff = 8
+opt.sidescrolloff = 8
+
+opt.smoothscroll = true
+
+-- font family for neovide
+opt.guifont = { "Monolisa Nerd Font Mono", ":h12" }
+
+-- height prompt command
+opt.cmdheight = 0
+
+-- window same width
+opt.equalalways = true
+
+-- set plit opening windows
+opt.splitbelow = true
+opt.splitright = true
+
+-- test indent linebreak
+opt.breakindent = true
+vim.opt.showbreak = string.rep(" ", 3) -- Make it so that long lines wrap smartly
+opt.linebreak = true
+
+opt.clipboard = "unnamedplus"
+
+vim.diagnostic.config({ virtual_text = false })
+
+-- Set background blurry
+-- vim.api.nvim_command([[
+--      augroup ChangeBackgroudColour
+--        autocmd colorscheme * :hi normal guibg=#0d1926b8
+--      augroup END
+-- ]])
+
+-- Set colorscheme
+opt.termguicolors = true
+
+vim.cmd.colorscheme 'catppuccin-latte'
+
+-- -- set back up and swap file directory
+opt.swapfile = false
+-- opt.backup = false
+local home = os.getenv("HOME")
+local backupDir = home .. "/.nvim/tmp/"
+vim.opt.directory = backupDir
+opt.undofile = true
+
+--lualine setup
+require('lualine').setup {
+	options = {
+		icons_enabled = true,
+		theme = 'auto',
+		component_separators = { left = '', right = '' },
+		section_separators = { left = '', right = '' },
+		disabled_filetypes = {
+			statusline = {},
+			winbar = {},
+		},
+		ignore_focus = {},
+		always_divide_middle = true,
+		globalstatus = false,
+		refresh = {
+			statusline = 1000,
+			tabline = 1000,
+			winbar = 1000,
+		}
+	},
+	sections = {
+		lualine_a = { 'mode' },
+		lualine_b = { 'branch', },
+		lualine_c = { 'filename' },
+		lualine_x = { 'diff', '' },
+		lualine_y = {
+			{
+				'datetime',
+				-- options: default, us, uk, iso, or your own format string ("%H:%M", etc..)
+				style = '%d/%m %H:%M'
+			}
+		},
+		lualine_z = { 'location' },
+	},
+}
+require("bufferline").setup{
+	keys = {
+    { "<leader>bp", "<Cmd>BufferLineTogglePin<CR>", desc = "Toggle pin" },
+    { "<leader>bP", "<Cmd>BufferLineGroupClose ungrouped<CR>", desc = "Delete non-pinned buffers" },
+    { "<S-h>", "<cmd>BufferLineCyclePrev<cr>", desc = "Prev buffer" },
+    { "<S-l>", "<cmd>BufferLineCycleNext<cr>", desc = "Next buffer" },
+  },
+  version = "*",
+  opts = {
+    options = {
+      -- stylua: ignore
+      close_command = function(n) require("mini.bufremove").delete(n, false) end,
+      -- stylua: ignore
+      right_mouse_command = function(n) require("mini.bufremove").delete(n, false) end,
+      diagnostics = "nvim_lsp",
+      always_show_bufferline = true,
+      offsets = {
+        {
+          filetype = "neo-tree",
+          text = "Neo-tree",
+          highlight = "Directory",
+          text_align = "left",
+        },
+      },
+    },
+  },
+}
